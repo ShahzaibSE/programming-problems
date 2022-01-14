7d898ec62ec2715f2b1e9bedcc7d0edebec725f9
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-01-14 23:04:40 +0500

    Updated

---
+++ a-very-big-sum.py
@@ -0,0 +1,5 @@
+def aVeryBigSum(ar):
+    # Write your code here
+    return sum(ar)
+
+print(aVeryBigSum([num for num in range(100)]))    
---
+++ bitwise-operations.py
@@ -0,0 +1,6 @@
+val = 1
+print("On left shift")
+print(val << 2)
+print(val << 1)
+print("On right shift")
+print(val >> 2)
