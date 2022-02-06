55884b6b0120e98c7d292c5cbc2398bee965b1f5
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-02-06 19:11:51 +0500

    Updated

--- twoSets.py
+++ twoSets.py
@@ -8,5 +8,14 @@
 #
 
 def getTotalX(a, b):
-    # Write your code here
-    return 1
+    # Write your code here 
+    a_max = max(a)
+    b_min = min(b)
+    count = 0
+    for num in range(a_max, b_min+1):
+        left = all([num % numA == 0 for numA in a])
+        right = all([numB % num == 0 for numB in b])
+        count += left * right
+    return count
+
+print(getTotalX([2,6], [24,36]))
