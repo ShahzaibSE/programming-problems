486e6566ccc88f935a22b87fae40eff039bb4d7c
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-04-20 17:44:23 +0500

    Updated

---
+++ hackerRank/loveMystery.py
@@ -0,0 +1,31 @@
+# James found a love letter that his friend Harry has written to his girlfriend. James is a prankster, so he decides to meddle with the letter. He changes all the words in the letter into palindromes.
+
+# To do this, he follows two rules:
+
+# He can only reduce the value of a letter by , i.e. he can change d to c, but he cannot change c to d or d to b.
+# The letter  may not be reduced any further.
+# Each reduction in the value of any letter is counted as a single operation. Find the minimum number of operations required to convert a given string into a palindrome.
+
+# Example
+
+# The following two operations are performed: cde → cdd → cdc. Return .
+
+# Function Description
+
+# Complete the theLoveLetterMystery function in the editor below.
+
+# theLoveLetterMystery has the following parameter(s):
+
+# string s: the text of the letter
+# Returns
+
+# int: the minimum number of operations
+
+def theLoveLetterMystery(s):
+    for index in range(len(s)):
+        if(s[index] == 'a'):
+            continue
+        print(chr(ord(s[index])))
+
+
+print(theLoveLetterMystery('cde'))
---
+++ hackerRank/maxArraySum.py
@@ -0,0 +1,40 @@
+# Given an array of integers, find the subset of non-adjacent elements with the maximum sum. Calculate the sum of that subset. It is possible that the maximum sum is , the case when all elements are negative.
+
+# Example
+# arr = [-2,1,3,-4,5]
+
+# The following subsets with more than  element exist. These exclude the empty subset and single element subsets which are also valid.
+
+# Subset      Sum
+# [-2, 3, 5]   6
+# [-2, 3]      1
+# [-2, -4]    -6
+# [-2, 5]      3
+# [1, -4]     -3
+# [1, 5]       6
+# [3, 5]       8
+
+# The maximum subset sum is . Note that any individual element is a subset as well.
+
+
+# In this case, it is best to choose no element: return .
+
+# Function Description
+
+# Complete the  function in the editor below.
+
+# maxSubsetSum has the following parameter(s):
+
+# int arr[n]: an array of integers
+# Returns
+# - int: the maximum subset sum
+
+# Input Format
+
+# The first line contains an integer, .
+# The second line contains  space-separated integers .
+
+# Constraints
+
+def maxSubsetSum(arr):
+    pass
